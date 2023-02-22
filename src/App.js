556c588a6a9aec22794eec621e00977bea0ed405
Taras.Kravets <taraskravets95@gmail.com>
2023-02-22 17:06:34 +0200

    feat(UI): added layout to card/modal/backdrop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Todo from './components/Todo';
 
 function App() {
-	return <></>;
+	return (
+		<div className='app'>
+			<h1>My Todos</h1>
+			<Todo text='Learn React' />
+			<Todo text='Master React' />
+			<Todo text='Explore the full React course' />
+		</div>
+	);
 }
 
 export default App;
