e6553cb25537adca68ec8832da79c4f14d270e15
Taras.Kravets <taraskravets95@gmail.com>
2023-02-23 10:34:43 +0200

    feat: (UI-header):  added badge with count of favorites

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import FavoritesContext from '../../store/favoriteContext';
 import styles from '../../styles/components/layout/MainNavigation.module.css';
 
 function MainNavigation() {
+	const favoritesCtx = useContext(FavoritesContext);
+
 	return (
 		<header className={styles.header}>
 			<div className={styles.logo}>React Meetups</div>
@@ -16,7 +19,12 @@ function MainNavigation() {
 						<NavLink to='/new-meetup'>Add New Meetups</NavLink>
 					</li>
 					<li>
-						<NavLink to='/favorite'>Favorite Meetups</NavLink>
+						<NavLink to='/favorite'>
+							Favorite Meetups
+							<span className={styles.badge}>
+								{favoritesCtx.totalFavorites}
+							</span>
+						</NavLink>
 					</li>
 				</ul>
 			</nav>
